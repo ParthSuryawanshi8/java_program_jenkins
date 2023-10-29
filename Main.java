7d8829ed260aad9c961cbc209b79db0e8bdd9a95
ParthSuryawanshi8 <suryawanshiparth8@gmail.com>
2023-10-29 20:10:36 +0530

    added student class

--- Main.java
+++ Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println(new Date());
         System.out.println("hello");
         System.out.println("ADDED ONE MORE FILE");
+        Student student = new Student();
+        student.sayHello();
     }
 }
